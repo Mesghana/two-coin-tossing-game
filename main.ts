f12e4799e7c7cda1463121c5945bcc0295d0b190
Mesghana <101273285+Mesghana@users.noreply.github.com>
2022-03-09 11:15:33 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,19 @@
+input.onButtonPressed(Button.A, function () {
+    player_1 = randint(0, 1)
+    if (coin_1 == coin_2 && coin_2 == coin_1) {
+        player_1 += 1
+        basic.showString("" + (player_1))
+    } else if (0 == 0 || 0 == 0) {
+    	
+    }
+})
+let coin_2 = 0
+let coin_1 = 0
+let player_1 = 0
+player_1 = 0
+let player_2 = 0
+coin_1 = 0
+coin_2 = 0
+basic.forever(function () {
+	
+})
